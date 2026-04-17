15c46fea3af36c7a7dcbacb6c518b88539f8266a
Gabriel <2866037@alu.murciaeduca.es>
2026-04-17 13:43:14 +0200

    pruebas con curl unidad 3 pasadas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             ModulosSeeder::class,
             ResultadosAprendizajeSeeder::class,
             CriteriosEvaluacionSeeder::class,
+            EcosistemaLaboralSeeder::class,
         ]);
 
         // User::factory(10)->create();
